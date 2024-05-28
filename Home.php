b6b19611e36e5956b1a37abc0b4f262f5ffb95b4
Arpo Roy <119074897+arpo008@users.noreply.github.com>
2024-05-28 16:06:25 +0600

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="topnav">
+        <a href="Signup.php">  Sign Up  </a>
+        <a href="login.php"> Login </a>
+        <a href="about.php">  About  </a>
+      </div>
+   
+        <div class="top">
+    <p > Welcome To Online Shopping Platfrom </p>
+</div>
+<div class = "h1">
+    <h1> BNBD </h1>
+</div>
+<div class= "a">
+    <p > Unique Shopping Experience  </p>
+    <p >* VISIT <br> * ORDER<br> * ENJOY YOUR DAY</p>
+   
+    </div>
+</body>
+
+</html>
