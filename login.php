304561b5714e566a26b49d021ebea27dcb014c4e
Tayhan87 <tayhanhossain87@gmail.com>
2024-05-31 20:13:56 +0600

    updated

--- login.php
+++ login.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="login">
         <h1>LOG IN</h1>
-        <p>Please enter your valid email and password</p>
+        <p class="h5">Please enter your valid email and password</p>
         <form action="login db.php" method="post">
             <label>Email</label>
             <input type="email" name="email" required>
             <label>Password</label>
             <input type="password" name="password" required>
-            <input type="submit" name=" " value="Log In">
+            <input type="submit" name=" " class="p-1 mt-3 mb-2" value="Log In">
         </form>
         <p>Not have an account? Click <a href="Signup.php">Sign Up Here</a></p>
         <p><a href="Home.php">Home</a></p>
