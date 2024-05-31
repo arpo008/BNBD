21d4b66d3d3857e9b579aed68f9394df886e89ae
Arpo Roy <119074897+arpo008@users.noreply.github.com>
2024-06-01 01:25:28 +0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,7 @@
 <?php
  $con = mysqli_connect('localhost', 'root', '', 'bnbd');
  echo $ID= $_GET['id'];
+ mysqli_query($con , "DELETE FROM product WHERE id= $ID");
+ header('location:product.php');
 
 ?>
