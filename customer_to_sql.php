f7b87ec97037e058fa39e427bccaa6b1b5c157d8
Tayhan87 <tayhanhossain87@gmail.com>
2024-06-12 03:21:36 +0600

    update

--- customer_to_sql.php
+++ customer_to_sql.php
@@ -7,12 +7,54 @@
    echo " No Data Entry";
    exit;
  }   
+
+ $total=0;
  $ar = json_decode($ar[0],true);
  foreach($ar as $value){
   echo $value['pid']."<br>";
   echo $value['quantity']."<br>"; 
   echo $value['totalprice']."<br>";
+  $total+=$value['totalprice'];
  }
+
  $cid=$_SESSION['cid'];
  echo "Customer ID is " . $cid;
+ date_default_timezone_set("Asia/Dhaka");
+ $t1=date("hidm");
+ $time=date("d/m/Y");
+
+$date = DateTime::createFromFormat("d/m/Y", $time);
+$date->modify('+7 days');
+$newdate = $date->format("d/m/Y");
+ 
+ $oid=$t1.$cid;
+ echo "<br>".$oid."<br>".$t1;
+
+ $con = mysqli_connect('localhost', 'root', '', 'bnbd');
+ 
+
+
+
+     $sql = "INSERT INTO orders VALUES ('$cid','$oid','$time','$newdate','$total')";
+
+     foreach($ar as $value){ 
+      $pid=$value['pid'];
+      $quantity=$value['quantity'];
+      $sql2="INSERT INTO order_detail VALUES ('$oid','$pid','$quantity')";
+      if (mysqli_query($con, $sql2)) {
+        //header('location: product.php');
+        echo "<br>Data added in order_detail <br>";
+     } else {
+         echo "Error inserting data: " . mysqli_error($con);
+     }
+     }   
+     
+    
+     if (mysqli_query($con, $sql)) {
+        //header('location: product.php');
+        echo "<br>Data added in orders <br>";
+     } else {
+         echo "Error inserting data: " . mysqli_error($con);
+     }
+
 ?>
